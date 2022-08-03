6441596e769068fc2cb1c597e0bf46ce33e81f04
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-03 20:26:14 +0800

    Delete Config directory

--- Config/Config.hs
+++
@@ -1,9 +0,0 @@
-module Config where
-
-import IHP.Prelude
-import IHP.Environment
-import IHP.FrameworkConfig
-
-instance FrameworkConfig where 
-    environment = Development
-    appHostname = "localhost"
